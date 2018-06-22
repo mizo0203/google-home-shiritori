ff994ca0bdc161471da16f6229d32e217a0caf45
hayatedayon <hayatedayon@gmail.com>
2018-06-22 23:42:25 +0900

    feat(*.py) Python 2/3 compatible code. fix #388 @0.5h

--- main_test.py
+++ main_test.py
@@ -12,9 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import webtest
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import main
+
+import webtest
 
 
 def test_get():
