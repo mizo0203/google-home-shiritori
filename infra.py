40fef61a919bc8b8873659f54b0e2e303b0bcab1
hayatedayon <hayatedayon@users.noreply.github.com>
2018-07-21 18:09:41 +0900

    feat(infra.py): fix PEP8. fix #399 @0.5h

--- infra.py
+++ infra.py
@@ -1,4 +1,20 @@
-#!/usr/local/bin/python3
+# Copyright 2018 [name of copyright owner]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import json
 
@@ -7,19 +23,14 @@ with open('data/dict.json') as json_file:
 
 
 def search_reading_from_dic(search_word):
-    """
-    json形式の辞書ファイルを全探索し、引数の文字列の読みをカタカナで返す
-    引数 : 検索する文字列
-    戻り値 : 検索した文字列のカタカナ読み(辞書にない場合は'')
+    """json形式の辞書ファイルを全探索し、引数の文字列の読みをカタカナで返す
+
+    :param str search_word: 検索する文字列
+    :rtype: str
+    :return: 検索した文字列のカタカナ読み(辞書にない場合は'')
     """
     for dict_record in json_dic:
         for word in dict_record['org']:
             if(word == search_word):
                 return dict_record['key']
     return ''
-
-
-if __name__ == '__main__':
-    print(search_reading_from_dic(u'リンゴ'))
-    print(search_reading_from_dic(u'林檎'))
-    print(search_reading_from_dic(u'りんご'))
