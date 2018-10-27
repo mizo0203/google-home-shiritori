c6216a37309784ec85e6e188c4f1c4327867e000
hayatedayon <hayatedayon@users.noreply.github.com>
2018-10-27 19:51:54 +0900

    feat(*): Add support for anime dictionary. fix #444 @0.5h

--- tool/make_json_dict.py
+++ tool/make_json_dict.py
@@ -76,6 +76,7 @@ def kana_judge(c):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=u'JSON辞書作成スクリプト')
     parser.add_argument('-d', '--debug', action='store_true', help=u'デバッグ出力用オプション')
+    parser.add_argument('-a', '--animedb', action='store_true', help=u'anilogia/animedbの読み込み時に使用')
     parser.add_argument('-b', '--biology', action='store_true', help=u'生物学の学名と和名の対応ファイルの読み込み時に使用')
     parser.add_argument('-n', '--naistdic', action='store_true', help=u'NAIST Japanese Dictionaryの読み込み時に使用')
     parser.add_argument('-p', '--pokemon', action='store_true', help=u'kotofurumiya/pokemon_dataの読み込み時に使用')
@@ -136,6 +137,16 @@ if __name__ == '__main__':
                         for tmp in HIRAGANA_TO_KATAKANA.keys():
                             word = word.replace(tmp, HIRAGANA_TO_KATAKANA[tmp])
                     inputData[word.replace(u'・', '').replace(' ', '').replace(u'亜種', u'アシュ')] = [word]
+        elif args.animedb:
+            obj = csv.reader(f)
+            for line in obj:
+                org = line[4]
+                key = line[6].replace(' ', '')
+                if key in inputData:
+                    if org not in inputData[key]:
+                        inputData[key].append(org)
+                else:
+                    inputData[key] = [org]
         else:
             sys.exit(u'オプションが指定されていません')
     print(u'inputData  length:', len(inputData))
